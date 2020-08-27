7d8c1723b595c7eadb8d858e37bf35abe47e2294
Kunaal Ahuja <kunaal92@gmail.com>
2020-08-27 02:05:10 -0400

    fix the previous commit

--- js/template.js
+++ js/template.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($) {
 	
 	// our function that decides weather the navigation bar should have "fixed" css position or not.
 	var sticky_navigation = function(){
-		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+		var scroll_top = $(window).scrollTop() *2; // our current vertical position from the top
 		
 		// if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
 		if (scroll_top > sticky_navigation_offset_top) { 
